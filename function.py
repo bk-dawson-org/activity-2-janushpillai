252b1d0cae4813dd512040dadf70c503d193a053
janushpillai <janush.kailayapillai@dawsoncollege.qc.ca>
2024-09-13 23:14:15 +0000

    Gommale

---
+++ function.py
@@ -0,0 +1,8 @@
+
+def calculatearea(radius):
+    area= 3.14*radius**2
+    return area
+
+radius=float(input("what is the radius of the circle?:"))
+x=calculatearea(radius)
+print(x)
---
+++ tax.py
@@ -0,0 +1,6 @@
+OG=float(input("what is the price of the total meal?:"))
+OGtax=OG*1.18
+print("this is the price of the meal including tax:", round(OGtax,0))
+
+x= "gommale"*3
+print(x)
